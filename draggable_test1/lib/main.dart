7c5a608f571c34df4de0123ae4475c8548fecfc1
Shtirlits1972 <shtirlits1972@gmail.com>
2023-04-18 16:29:35 +0300

    initial

---
+++ draggable_test1/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:draggable_test1/statful_vidget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('MyApp'),
+            centerTitle: true,
+          ),
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MyStatefulWidget(
+                  dragText: '1',
+                  targetText: 'ONE',
+                ),
+                MyStatefulWidget(
+                  dragText: '2',
+                  targetText: 'TWO',
+                ),
+              ])),
+    );
+  }
+}
---
+++ draggable_test1/lib/statful_vidget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget(
+      {super.key, required this.dragText, required this.targetText});
+  int idWidget = 0;
+  String dragText = '';
+  String targetText = '';
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  Color dragColor = Colors.blueAccent;
+  Color targetColor = Colors.yellowAccent;
+
+  String dragTextCurr = '';
+  String targetTextCurr = '';
+
+  @override
+  void initState() {
+    super.initState();
+    dragTextCurr = widget.dragText;
+    targetTextCurr = widget.targetText;
+    print('widget.dragText : ${widget.dragText}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Draggable<int>(
+          // Data is the value this Draggable stores.
+          data: 1,
+          //   axis: Axis.horizontal,
+          feedback: Container(
+            height: 100.0,
+            width: 100.0,
+            color: dragColor,
+            child: Center(
+              child: Text(
+                dragTextCurr,
+                style: TextStyle(color: Colors.black, fontSize: 30),
+              ),
+            ),
+          ),
+          onDragStarted: () {
+            dragTextCurr = '';
+            dragColor = Colors.transparent;
+            print(widget.dragText);
+          },
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            color: dragColor,
+            child: Center(
+              child: Text(
+                dragTextCurr,
+                style: const TextStyle(color: Colors.black, fontSize: 30),
+              ),
+            ),
+          ),
+        ),
+        DragTarget<int>(
+          builder: (
+            BuildContext context,
+            List<dynamic> accepted,
+            List<dynamic> rejected,
+          ) {
+            return Container(
+              height: 100.0,
+              width: 100.0,
+              color: targetColor,
+              child: Center(
+                child: Text(
+                  targetTextCurr,
+                  style: const TextStyle(color: Colors.black, fontSize: 30),
+                ),
+              ),
+            );
+          },
+          onAccept: (int data) {
+            print(widget.dragText);
+
+            setState(() {
+              targetTextCurr = widget.dragText;
+              targetColor = Colors.blueAccent;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
